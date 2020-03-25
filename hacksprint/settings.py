879b5e134feeaecac88886a5b64fa484c03fd7df
shakilahmmeed <shakilahmmeed@gmail.com>
2020-03-25 21:34:32 +0600

    theme updated

--- hacksprint/settings.py
+++ hacksprint/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig'
 ]
 
 MIDDLEWARE = [
@@ -88,3 +89,7 @@ USE_TZ = True
 STATIC_URL = '/assets/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
