1cac2fa5d3d85db9b57d5b19332ea035ab2f1c8b
Tahsin Iftekhar <48128233+tahsiniftekhar@users.noreply.github.com>
2020-06-07 22:32:07 +0600

    Adminpanel Users CRUD System Added (#28)

--- adminpanel/views.py
+++ adminpanel/views.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required, user_passes_test
-from django.contrib import auth
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
 from cloudinary import uploader
 
 from adminpanel.form import PracticeForm
@@ -17,10 +17,67 @@ def index(request):
     return render(request, 'adminpanel/index.html')
 
 
+# Users Page
 @user_passes_test(lambda u: u.is_superuser)
 def users(request):
     all_user = User.objects.all().order_by('-date_joined')
     return render(request, 'adminpanel/users.html', {'users': all_user})
+
+# Create User
+@user_passes_test(lambda u: u.is_superuser)
+@csrf_exempt
+def createUser(request):
+    firstName = request.POST["firstName"]
+    lastName = request.POST["lastName"]
+    userName = request.POST["userName"]
+    email = request.POST["email"]
+    password = request.POST["password"]
+
+    try:
+        user = User.objects.create_user(first_name=firstName, last_name=lastName, username=userName, email=email, password=password)
+        user_data={'id':user.id,'firstName':user.first_name,'lastName':user.last_name,'userName':user.username,'email':user.email,'last_login':user.last_login,"error":False,"errorMessage":"User Added Successfully!"}
+        return JsonResponse(user_data,safe=False)
+    except:
+        user_data={"error":True,"errorMessage":"Failed to Add User!"}
+        return JsonResponse(user_data,safe=False)
+
+# Edit User
+@user_passes_test(lambda u: u.is_superuser)
+@csrf_exempt
+def editUser(request):
+    id = request.POST["id"]
+    firstName = request.POST["firstName"]
+    lastName = request.POST["lastName"]
+    userName = request.POST["userName"]
+    email = request.POST["email"]
+
+    try:
+        user=User.objects.get(id=id)
+        user.first_name = firstName
+        user.last_name = lastName
+        user.username = userName
+        user.email = email
+        user.save()
+
+        user_data={'id':user.id,'firstName':user.first_name,'lastName':user.last_name,'userName':user.username,'email':user.email,'last_login':user.last_login,"error":False,"errorMessage":"User Updated Successfully!"}
+        return JsonResponse(user_data,safe=False)
+    except:
+        user_data={"error":True,"errorMessage":"Failed to Update User!"}
+        return JsonResponse(user_data,safe=False)
+
+# Delete User
+@user_passes_test(lambda u: u.is_superuser)
+@csrf_exempt
+def deleteUser(request):
+    id = request.POST["id"]
+    try:
+        user=User.objects.get(id=id)
+        user.delete()
+        user_data={"error":False,"errorMessage":"User Deleted Successfully!"}
+        return JsonResponse(user_data,safe=False)
+    except:
+        user_data={"error":True,"errorMessage":"Failed to Delete User!"}
+        return JsonResponse(user_data,safe=False)
 
 
 @user_passes_test(lambda user: user.is_superuser or user.is_staff)
