65d3250919fae0d18d87f72c9facedb3ec934d5c
shakilahmmeed <shakilahmmeed@gmail.com>
2020-07-13 10:27:29 +0600

    Update env-sample.py

--- env-sample.py
+++ env-sample.py
@@ -16,6 +16,7 @@ DB_CONFIG = {
     'ENGINE': 'django.db.backends.sqlite3',
     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 }
+
 # DB_CONFIG = {
 #     'ENGINE': 'django.db.backends.mysql',
 #     'NAME': 'hacksprintdb',
