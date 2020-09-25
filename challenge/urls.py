3d35635b46a7e76976524b18f8558a5bb2afbc9f
shakilahmmeed <shakilahmmeed@gmail.com>
2020-09-25 09:24:49 +0600

    fix: domain endpoint issue fixed

--- challenge/urls.py
+++ challenge/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ChallengeView.as_view()),
+    path('domains/', views.DomainView.as_view()),
     path('<slug:slug>/', views.ChallengeDetailView.as_view()),
-    path('domains/', views.DomainView.as_view())
 ]
